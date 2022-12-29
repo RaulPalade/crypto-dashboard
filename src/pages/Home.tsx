35e1fc43088fee4b4e09994fb83e03b931fb4b7c
Raul Palade <raul.palade@edu.unito.it>
2022-12-29 11:29:37 +0100

    [ADD] Minicharts, advanced chart, economic calendar and crypto currency markert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
-import { MiniChart } from "react-ts-tradingview-widgets";
+import {
+  MiniChart,
+  AdvancedRealTimeChart,
+  EconomicCalendar,
+  CryptoCurrencyMarket,
+} from "react-ts-tradingview-widgets";
 import { useEffect } from "react";
 
 function Home() {
@@ -19,20 +24,33 @@ function Home() {
 
   return (
     <section className="home-section">
-      <div className="home-content">
-        <div>
-          <MiniChart
-            colorTheme="dark"
+      <div className="home-content flex w-full flex-col">
+        <div className="mx-auto grid w-full grid-cols-1 gap-4 p-8 sm:grid-cols-2 sm:gap-5 lg:grid-cols-4 lg:gap-10">
+          <MiniChart colorTheme="dark" symbol="BTC/USD" autosize></MiniChart>
+          <MiniChart colorTheme="dark" symbol="ETH/USD" autosize></MiniChart>
+          <MiniChart colorTheme="dark" symbol="BNB/USD" autosize></MiniChart>
+          <MiniChart colorTheme="dark" symbol="MATIC/USD" autosize></MiniChart>
+        </div>
+        <div className="mx-auto w-full pl-8 pr-8">
+          <AdvancedRealTimeChart
+            theme="dark"
             width="100%"
-            symbol="BTC/USD"></MiniChart>
-          <MiniChart
-            colorTheme="dark"
-            width="100%"
-            symbol="ETH/USD"></MiniChart>
-          <MiniChart
-            colorTheme="dark"
-            width="100%"
-            symbol="BNB/USD"></MiniChart>
+            symbol="BTC/USD"
+            height={550}></AdvancedRealTimeChart>
+        </div>
+        <div className="mx-auto grid w-full grid-cols-1 gap-4 p-8 xl:grid-cols-3 xl:gap-10">
+          <div className="xl:col-span-1">
+            <EconomicCalendar
+              colorTheme="dark"
+              width="100%"
+              height={600}></EconomicCalendar>
+          </div>
+          <div className="xl:col-span-2">
+            <CryptoCurrencyMarket
+              colorTheme="dark"
+              width="100%"
+              height={600}></CryptoCurrencyMarket>
+          </div>
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xs: "1100px",
+      xm: "1200px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
