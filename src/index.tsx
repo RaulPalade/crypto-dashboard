641f663eace0a055b1dcf3cd505e1befef78eb7a
Raul Palade <raulpalade@outlook.com>
2023-03-26 00:54:04 +0100

    Added news card and found API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./style_news.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
