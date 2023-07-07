eb7c42a72f40dabfb00049bccdc6ff68ef6ba310
Raul Palade <raulpalade@outlook.com>
2023-07-07 11:25:27 +0300

    Css code cleaning

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./style_news.css";
+import "./style/page_structure.css";
+import "./style/sidebar.css";
+import "./style/table.css";
+import "./style/chart.css";
+import "./style/modal_coin_list.css";
+import "./style/arbitrage_card.css";
+import "./style/mining_card.css";
+import "./style/style_news.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
